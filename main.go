0021af12f417bff29a670980171ecd0065dfdd14
Nicolas Long <nicolaslong@gmail.com>
2018-02-28 10:17:43 +0000

    Add working implementation
    Note, at the moment this is *not* production ready:
    
    * it is likely insecure - for example oauth 'state' is hardcoded
    * the code is ugly and generally not configurable
    
    Having said that, I'm committing because it 'works' in a minimal
    sense.

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"io/ioutil"
@@ -11,7 +13,11 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 )
+
+// For info on JWT see: https://tools.ietf.org/html/rfc7519
+// For info on oauth see: ..
 
 type gPlusResp struct {
 	Email         string `json:"email"`
@@ -37,6 +43,29 @@ func main() {
 	fmt.Println(http.ListenAndServe(":3000", nil))
 }
 
+func proxyWithAuth(w http.ResponseWriter, r *http.Request) {
+	g, _ := url.Parse("https://www.theguardian.com")
+	proxy := httputil.NewSingleHostReverseProxy(g)
+
+	cookies := r.Cookies()
+	var session string
+	for _, c := range cookies {
+		if c.Name == "session" {
+			session = c.Value
+		}
+	}
+
+	_, err := getEmailFromToken(session, []byte("foo"))
+	if err != nil {
+		state := "foo" // TODO randomise and confirm other side (CSRF)
+		http.Redirect(w, r, oauthConf.AuthCodeURL(state), http.StatusFound)
+		return
+	}
+
+	r.Host = r.URL.Host // TODO remind myself why this works
+	proxy.ServeHTTP(w, r)
+}
+
 func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	// TODO check state matches
 	code := r.FormValue("code")
@@ -45,7 +74,8 @@ func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	client := oauthConf.Client(oauth2.NoContext, tok)
 	resp, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
 	if err != nil {
-		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(fmt.Sprintf("Unauthorised (%s)", err.Error())))
 		return
 	}
 	defer resp.Body.Close()
@@ -54,105 +84,61 @@ func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	var userInfo gPlusResp
 	err = json.Unmarshal(body, &userInfo)
 
-	fmt.Println(string(body))
-
 	email := userInfo.Email
 	parts := strings.SplitAfter(email, "@")
 	domain := parts[len(parts)-1]
 
 	if domain != "guardian.co.uk" {
-		found := email + ", " + domain
-
 		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte(found))
-		fmt.Println(email, domain)
+		w.Write([]byte("Unauthorised (unsupported email domain)"))
 		return
 	}
 
-	// store jwt token in session
+	hmacSecret := []byte("foo")
+	sessionToken, err := newSessionToken(email, hmacSecret)
 
-	w.Write([]byte(domain))
-}
-
-func proxyWithAuth(w http.ResponseWriter, r *http.Request) {
-	g, _ := url.Parse("https://www.theguardian.com")
-	proxy := httputil.NewSingleHostReverseProxy(g)
-	state := "foo" // TODO randomise and confirm other side (CSRF)
-
-	http.Redirect(w, r, oauthConf.AuthCodeURL(state), http.StatusFound)
-
-	if isAuthed(r) {
-		r.Host = r.URL.Host
-		proxy.ServeHTTP(w, r)
-	} else {
-		auth()
+	session := http.Cookie{
+		Name:     "session",
+		Value:    sessionToken,
+		Domain:   "localhost",
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   false, // TODO make secure when on https
 	}
 
+	w.Header().Add("Set-Cookie", session.String())
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
-func isAuthed(r *http.Request) bool {
-	// check jwt token is signed correctly with expected method
-	// jwt header.payload.signature
-	//
-	return true
+func newSessionToken(email string, hmacSecret []byte) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": email,
+		"exp":   time.Now().AddDate(0, 0, 30).Unix(),
+	})
+
+	return token.SignedString(hmacSecret)
 }
 
-func auth() {
-	// build a 'client' from config
-	// use it to do auth flow
-	// then handle token as expected...
+func getEmailFromToken(token string, hmacSecret []byte) (string, error) {
+	userToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
 
-	// note, there is a double flow: 1) get auth code 2) get token
+		return hmacSecret, nil
+	})
+	if err != nil {
+		return "", fmt.Errorf("Unable to parse token: %s", err.Error())
+	}
 
-	// redirect to google
-	// then on callback validate against domain
-	// store relevant info in session as jwt token with app secret
+	claims, ok := userToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", errors.New("Unable to parse jwt claims")
+	}
 
+	if !userToken.Valid {
+		return "", errors.New("Token is not valid (perhaps it has expired?)")
+	}
+
+	return claims["email"].(string), nil
 }
-
-// func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
-// 	url := googleOauthConfig.AuthCodeURL(oauthStateString)
-// 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-// }
-
-// func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
-// 	state := r.FormValue("state")
-// 	if state != oauthStateString {
-// 		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
-// 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-// 		return
-// 	}
-
-// 	code := r.FormValue("code")
-// 	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
-// 	if err != nil {
-// 		fmt.Printf("Code exchange failed with '%s'\n", err)
-// 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-// 		return
-// 	}
-
-// 	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
-// 	if err != nil {
-// 		// handle error
-// 	}
-// 	defer response.Body.Close()
-
-// 	contents, err := ioutil.ReadAll(response.Body)
-// 	fmt.Fprintf(w, "Content: %s\n", contents)
-
-// }
-
-// cli option to filter tasks being run
-// this proxy in front of kibana for security
-
-// lambda to ingest data from sns
-// lambda to write data to sns
-// note, configurable per task (to include only what is needed for that env)
-// we will run in a lambda but also tc scheduled task
-
-// note this requires:
-
-// cli flag for app to control tasks (comma-separated list?)
-
-// some way to view kibana stats..
-// okay to be ip only because no pi data!
